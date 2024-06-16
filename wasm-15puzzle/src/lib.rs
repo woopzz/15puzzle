288b7d5d85be6e0d81660468643f9b7ce92ee000
woopzz <bkovtunovych@gmail.com>
2024-06-16 16:27:36 +0300

    Refactor tests to be able to run them by "cargo test"

--- wasm-15puzzle/src/lib.rs
+++ wasm-15puzzle/src/lib.rs
@@ -73,60 +73,61 @@ fn calc_inversions_count(board: &str) -> u16 {
     return count;
 }
 
-fn main() {
-    // =====================================
-    // should check that a tile can be moved
-    // =====================================
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let b1 = Board {
-        state: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'],
-        path: vec![],
-    };
-    assert_eq!(b1.check_step(0), false);
-    assert_eq!(b1.check_step(10), false);
-    assert_eq!(b1.check_step(100), false);
+    #[test]
+    fn should_check_that_a_tile_can_be_moved() {
+        let b1 = Board {
+            state: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'],
+            path: vec![],
+        };
+        assert_eq!(b1.check_step(0), false);
+        assert_eq!(b1.check_step(10), false);
+        assert_eq!(b1.check_step(100), false);
 
-    // 0 1 2 3
-    // 4 5 6 7
-    // 8 9 F A
-    // C D E B
-    let b2 = Board {
-        state: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'F', 'A', 'C', 'D', 'E', 'B'],
-        path: vec![],
-    };
-    assert_eq!(b2.check_step(6), true);
-    assert_eq!(b2.check_step(9), true);
-    assert_eq!(b2.check_step(11), true);
-    assert_eq!(b2.check_step(14), true);
+        // 0 1 2 3
+        // 4 5 6 7
+        // 8 9 F A
+        // C D E B
+        let b2 = Board {
+            state: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'F', 'A', 'C', 'D', 'E', 'B'],
+            path: vec![],
+        };
+        assert_eq!(b2.check_step(6), true);
+        assert_eq!(b2.check_step(9), true);
+        assert_eq!(b2.check_step(11), true);
+        assert_eq!(b2.check_step(14), true);
+    }
 
-    // ========================================
-    // should replace a tile with the blank one
-    // ========================================
+    #[test]
+    fn should_replace_a_tile_with_the_blank_one() {
+        let b3 = Board {
+            state: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'],
+            path: vec![],
+        };
+        assert_eq!(b3.step(11).state, ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'F', 'C', 'D', 'E', 'B']);
+        assert_eq!(b3.step(14).state, ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'F', 'E']);
 
-    let b3 = Board {
-        state: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'],
-        path: vec![],
-    };
-    assert_eq!(b3.step(11).state, ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'F', 'C', 'D', 'E', 'B']);
-    assert_eq!(b3.step(14).state, ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'F', 'E']);
+        let b4 = Board {
+            state: ['F', '1', '2', '3', '0', '5', '6', '7', '4', '9', 'A', 'B', '8', 'C', 'D', 'E'],
+            path: vec![],
+        };
+        assert_eq!(b4.step(4).state, ['0', '1', '2', '3', 'F', '5', '6', '7', '4', '9', 'A', 'B', '8', 'C', 'D', 'E']);
+        assert_eq!(b4.step(1).state, ['1', 'F', '2', '3', '0', '5', '6', '7', '4', '9', 'A', 'B', '8', 'C', 'D', 'E']);
+    }
 
-    let b4 = Board {
-        state: ['F', '1', '2', '3', '0', '5', '6', '7', '4', '9', 'A', 'B', '8', 'C', 'D', 'E'],
-        path: vec![],
-    };
-    assert_eq!(b4.step(4).state, ['0', '1', '2', '3', 'F', '5', '6', '7', '4', '9', 'A', 'B', '8', 'C', 'D', 'E']);
-    assert_eq!(b4.step(1).state, ['1', 'F', '2', '3', '0', '5', '6', '7', '4', '9', 'A', 'B', '8', 'C', 'D', 'E']);
+    #[test]
+    fn should_remember_moved_tiles_in_a_right_order() {
+        let b5 = Board {
+            state: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'],
+            path: vec![],
+        };
+        assert_eq!(
+            b5.step(14).step(10).step(9).step(13).step(14).path,
+            vec![14, 10, 9, 13, 14],
+        );
+    }
 
-    // ======================================================
-    // should remember moved tiles (indexes) in a right order
-    // ======================================================
-
-    let b5 = Board {
-        state: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'],
-        path: vec![],
-    };
-    assert_eq!(
-        b5.step(14).step(10).step(9).step(13).step(14).path,
-        vec![14, 10, 9, 13, 14],
-    );
 }
