e0f038a6094ee3a0c8081c323aa7394e4c5d0344
woopzz <bkovtunovych@gmail.com>
2024-06-19 00:42:36 +0300

    Autosolver

--- wasm-15puzzle/src/lib.rs
+++ wasm-15puzzle/src/lib.rs
@@ -1,3 +1,6 @@
+use std::cmp::Ordering;
+use std::collections::{HashSet, BinaryHeap};
+
 use rand::thread_rng;
 use rand::seq::SliceRandom;
 
@@ -86,6 +89,348 @@ impl Board {
         }
 
         return count;
+    }
+
+}
+
+struct LockedTiles {
+    state: [bool; 16],
+}
+
+impl LockedTiles {
+
+    fn new() -> LockedTiles {
+        let state = [
+            false, false, false, false,
+            false, false, false, false,
+            false, false, false, false,
+            false, false, false, false,
+        ];
+        return LockedTiles { state };
+    }
+
+    fn check_locked(&self, tile_index: TileIndex) -> bool {
+        return self.state[tile_index];
+    }
+
+    fn lock(&mut self, tile_index: TileIndex) {
+        self.state[tile_index] = true;
+    }
+
+}
+
+struct RankedBoard(i32, Board);
+
+impl Ord for RankedBoard {
+
+    fn cmp(&self, other: &Self) -> Ordering {
+        return self.0.cmp(&other.0);
+    }
+
+}
+
+impl PartialOrd for RankedBoard {
+
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        return Some(self.cmp(other));
+    }
+
+}
+
+impl PartialEq for RankedBoard {
+
+    fn eq(&self, other: &Self) -> bool {
+        return self.0 == other.0;
+    }
+
+}
+
+impl Eq for RankedBoard {}
+
+struct Autosolver {
+    board: Board,
+    locked_tiles: LockedTiles,
+}
+
+impl Autosolver {
+
+    fn new(board: Board) -> Autosolver {
+        let locked_tiles = LockedTiles::new();
+        return Autosolver { board, locked_tiles };
+    }
+
+    fn execute(&mut self) -> Board {
+        // TODO Fix it. It's not a good solution.
+        let mut board = Board { state: self.board.state, path: vec![] };
+
+        if !self.check_tiles_on_their_places(&board, HashSet::from(['0'])) {
+            board = self.bfs(board, PositionOneTile { tile: '0', dest_index: 0 });
+        }
+        self.locked_tiles.lock(0);
+
+        if !self.check_tiles_on_their_places(&board, HashSet::from(['1'])) {
+            board = self.bfs(board, PositionOneTile { tile: '1', dest_index: 1 });
+        }
+        self.locked_tiles.lock(1);
+
+        if !self.check_tiles_on_their_places(&board, HashSet::from(['4'])) {
+            board = self.bfs(board, PositionOneTile { tile: '4', dest_index: 4 });
+        }
+        self.locked_tiles.lock(4);
+
+        if !self.check_tiles_on_their_places(&board, HashSet::from(['5'])) {
+            board = self.bfs(board, PositionOneTile { tile: '5', dest_index: 5 });
+        }
+        self.locked_tiles.lock(5);
+
+        if !self.check_tiles_on_their_places(&board, HashSet::from(['2', '3'])) {
+            board = self.bfs(board, PositionManyTiles { tiles: vec!['2', '3', BLANK_TILE], dest_indexes: vec![3, 10, 6] });
+            board = self.apply_formula(board);
+        }
+        self.locked_tiles.lock(2);
+        self.locked_tiles.lock(3);
+
+        if !self.check_tiles_on_their_places(&board, HashSet::from(['6', '7'])) {
+            board = self.bfs(board, PositionManyTiles { tiles: vec!['6', '7', BLANK_TILE], dest_indexes: vec![7, 14, 10] });
+            board = self.apply_formula(board);
+        }
+        self.locked_tiles.lock(6);
+        self.locked_tiles.lock(7);
+
+        if !self.check_tiles_on_their_places(&board, HashSet::from(['8', '9', 'A', 'B', 'C', 'D', 'E', 'F'])) {
+            board = self.bfs(
+                board,
+                PositionManyTiles {
+                    tiles: vec!['8', '9', 'A', 'B', 'C', 'D', 'E', 'F'],
+                    dest_indexes: vec![8, 9, 10, 11, 12, 13, 14, 15],
+                },
+            );
+        }
+
+        return board;
+    }
+
+    fn check_tiles_on_their_places(&self, board: &Board, tiles: HashSet<char>) -> bool {
+        for (tile, solved_board_tile) in board.state.iter().zip(SOLVED_BOARD_STATE.iter()) {
+            if tiles.contains(solved_board_tile) && tile != solved_board_tile {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    fn bfs(&self, initial_board: Board, solve_strategy: impl SolveStrategy) -> Board {
+        let mut heap = BinaryHeap::new();
+        heap.push(RankedBoard(solve_strategy.calc_heuristic(&initial_board), initial_board));
+
+        let mut visited_states: HashSet<Tiles> = HashSet::new();
+
+        loop {
+            let board = heap.pop().unwrap().1;
+
+            if visited_states.contains(&board.state) {
+                continue;
+            }
+
+            if solve_strategy.check_solved(&board) {
+                return board;
+            }
+
+            for maybe_tile_index in [
+                self.calc_index_of_tile_above(&board),
+                self.calc_index_of_tile_on_the_left(&board),
+                self.calc_index_of_tile_below(&board),
+                self.calc_index_of_tile_on_the_right(&board),
+            ] {
+                if maybe_tile_index.is_none() {
+                    continue;
+                }
+                let tile_index = maybe_tile_index.unwrap();
+                if board.check_step(tile_index) && !self.locked_tiles.check_locked(tile_index) {
+                    let next_board = board.step(tile_index);
+                    if !visited_states.contains(&next_board.state) {
+                        heap.push(RankedBoard(solve_strategy.calc_heuristic(&next_board), next_board));
+                    }
+                }
+            }
+
+            visited_states.insert(board.state);
+        }
+    }
+
+    fn apply_formula(&self, mut board: Board) -> Board {
+        //  Sometimes to move a tile on its place you have to rotate a box 2x2.
+        //  It means you have to move the previously set tile. For example:
+        //
+        //  0 1 2 6
+        //  7 A F 5
+        //  C 8 3 4
+        //  B 9 E D
+        //
+        //  You have no way to put the tile '3' on its position wihout moving the tile '2'.
+        //  So here we manually perform a formula to put the last tile in a row on its place.
+        //
+        //  In order to do so, we require a special configuration:
+        //
+        //  x x x 2
+        //  x x F x
+        //  x x 3 x
+        //  x x x x
+        //
+        //  After this we'll get ...
+        //
+        //  x x 2 3
+        //  x x F x
+        //  x x x x
+        //  x x x x
+        //
+        //  It's true for the second row as well (tiles '6' and '7').
+        board = board.step(self.calc_index_of_tile_below(&board).unwrap());
+        board = board.step(self.calc_index_of_tile_on_the_right(&board).unwrap());
+        board = board.step(self.calc_index_of_tile_above(&board).unwrap());
+        board = board.step(self.calc_index_of_tile_above(&board).unwrap());
+        board = board.step(self.calc_index_of_tile_on_the_left(&board).unwrap());
+        board = board.step(self.calc_index_of_tile_below(&board).unwrap());
+        board = board.step(self.calc_index_of_tile_on_the_right(&board).unwrap());
+        board = board.step(self.calc_index_of_tile_above(&board).unwrap());
+        board = board.step(self.calc_index_of_tile_on_the_left(&board).unwrap());
+        board = board.step(self.calc_index_of_tile_below(&board).unwrap());
+        return board;
+    }
+
+    fn calc_index_of_tile_above(&self, board: &Board) -> Option<TileIndex> {
+        let blank_tile_index = board.get_blank_tile_index();
+        if blank_tile_index < 4 {
+            return None;
+        }
+
+        return Some(blank_tile_index - 4);
+    }
+
+    fn calc_index_of_tile_on_the_left(&self, board: &Board) -> Option<TileIndex> {
+        let blank_tile_index = board.get_blank_tile_index();
+        if blank_tile_index < 1 {
+            return None;
+        }
+
+        return Some(blank_tile_index - 1);
+    }
+
+    fn calc_index_of_tile_below(&self, board: &Board) -> Option<TileIndex> {
+        let blank_tile_index = board.get_blank_tile_index();
+        if blank_tile_index > 11 {
+            return None;
+        }
+
+        return Some(blank_tile_index + 4);
+    }
+
+    fn calc_index_of_tile_on_the_right(&self, board: &Board) -> Option<TileIndex> {
+        let blank_tile_index = board.get_blank_tile_index();
+        if blank_tile_index > 14 {
+            return None;
+        }
+
+        return Some(blank_tile_index + 1);
+    }
+
+}
+
+trait SolveStrategy {
+    fn check_solved(&self, board: &Board) -> bool;
+
+    fn calc_heuristic(&self, board: &Board) -> i32 {
+        return -self.calc_manhattan_distance(board);
+    }
+
+    fn calc_manhattan_distance(&self, board: &Board) -> i32;
+
+    fn near(&self, index1: TileIndex, index2: TileIndex) -> bool {
+        let col1 = self.calc_col(index1) as i32;
+        let row1 = self.calc_row(index1) as i32;
+
+        let col2 = self.calc_col(index2) as i32;
+        let row2 = self.calc_row(index2) as i32;
+
+        return
+            (col1 - col2).abs() == 1 && row1 == row2 ||
+            (row1 - row2).abs() == 1 && col1 == col2;
+    }
+
+    fn calc_col(&self, index: TileIndex) -> TileIndex {
+        return ((index as f32) / 4.0).floor() as TileIndex;
+    }
+
+    fn calc_row(&self, index: TileIndex) -> TileIndex {
+        return index % 4;
+    }
+}
+
+struct PositionOneTile {
+    tile: char,
+    dest_index: TileIndex,
+}
+
+impl SolveStrategy for PositionOneTile {
+
+    fn check_solved(&self, board: &Board) -> bool {
+        return board.state[self.dest_index] == self.tile;
+    }
+
+    fn calc_manhattan_distance(&self, board: &Board) -> i32 {
+        let blank_tile_index = board.get_blank_tile_index();
+        let index = board.state.iter().position(|&x| x == self.tile).unwrap();
+
+        let cur_col = self.calc_col(index) as i32;
+        let cur_row = self.calc_row(index) as i32;
+
+        let dest_col: i32;
+        let dest_row: i32;
+        if self.near(index, blank_tile_index) || self.tile == BLANK_TILE {
+            dest_col = self.calc_col(self.dest_index) as i32;
+            dest_row = self.calc_row(self.dest_index) as i32;
+        } else {
+            dest_col = self.calc_col(blank_tile_index) as i32;
+            dest_row = self.calc_row(blank_tile_index) as i32;
+        }
+
+        return (cur_col - dest_col).abs() + (cur_row - dest_row).abs();
+    }
+
+}
+
+struct PositionManyTiles {
+    tiles: Vec<char>,
+    dest_indexes: Vec<TileIndex>,
+}
+
+impl SolveStrategy for PositionManyTiles {
+
+    fn check_solved(&self, board: &Board) -> bool {
+        for (tile, dest_index) in self.tiles.iter().zip(self.dest_indexes.iter()) {
+            if *tile != board.state[*dest_index] {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    fn calc_manhattan_distance(&self, board: &Board) -> i32 {
+        let mut distance: i32 = 0;
+
+        for (tile, dest_index) in self.tiles.iter().zip(self.dest_indexes.iter()) {
+            let index = board.state.iter().position(|&x| x == *tile).unwrap();
+
+            let cur_col = self.calc_col(index) as i32;
+            let cur_row = self.calc_row(index) as i32;
+
+            let dest_col = self.calc_col(*dest_index) as i32;
+            let dest_row = self.calc_row(*dest_index) as i32;
+
+            distance += (cur_col - dest_col).abs() + (cur_row - dest_row).abs();
+        }
+
+        return distance;
     }
 
 }
@@ -179,6 +524,34 @@ mod tests {
             path: vec![],
         };
         assert!(board.shuffle().check_solvable());
+    }
+
+    #[test]
+    fn should_find_a_way_to_get_the_solved_board() {
+        let board = Board {
+            state: ['5', '0', '8', '9', '2', '3', 'D', '6', 'B', 'E', '1', 'A', 'F', 'C', '7', '4'],
+            path: vec![],
+        };
+        let mut autosolver = Autosolver::new(board);
+        let solved_board = autosolver.execute();
+        assert_eq!(solved_board.state, SOLVED_BOARD_STATE);
+
+        let mut board2 = Board {
+            state: ['5', '0', '8', '9', '2', '3', 'D', '6', 'B', 'E', '1', 'A', 'F', 'C', '7', '4'],
+            path: vec![],
+        };
+        for tile in solved_board.path {
+            board2 = board2.step(tile);
+        }
+        assert_eq!(board2.state, SOLVED_BOARD_STATE);
+    }
+
+    #[test]
+    fn should_return_an_empty_path_if_the_board_is_solved_in_the_first_place() {
+        let board = Board { state: SOLVED_BOARD_STATE.clone(), path: vec![] };
+        let mut autosolver = Autosolver::new(board);
+        let solved_board = autosolver.execute();
+        assert_eq!(solved_board.path.len(), 0);
     }
 
 }
